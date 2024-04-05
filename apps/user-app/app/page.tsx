a73b23a0df035b057a91a9b3fadccc74b9249f5c
Pratham271 <chauhanpratham22@gmail.com>
2024-04-05 15:55:31 +0530

    added recoil

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,10 +1,11 @@
 import { PrismaClient } from "@repo/db/client";
+import Balance from "../components/balance";
 const prisma = new PrismaClient() 
 
 export default function Page(): JSX.Element {
   return (
     <div className="text-3xl flex justify-center text-neutral-500">
-      Hi there
+      <Balance/>
     </div>
   );
 }
---
+++ packages/store/src/hooks/useBalance.ts
@@ -0,0 +1,8 @@
+import { useRecoilValue } from "recoil"
+import { balanceAtom } from "../atoms/balance"
+
+
+export const useBalance = () => {
+    const value = useRecoilValue(balanceAtom)
+    return value;
+}
