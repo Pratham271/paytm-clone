4cfccd65f52b5494abdfd7c7d1ea369a95771413
Pratham271 <chauhanpratham22@gmail.com>
2024-04-06 13:18:58 +0530

    added the bank-webhook in apps

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -2,6 +2,48 @@ import express from 'express'
 import db from '@repo/db/client'
 
 const app = express()
+app.use(express.json())
+
+app.post("/hdfcWebhook", async(req,res)=> {
+    const paymentInformation = {
+        token: req.body.token,
+        userId: req.body.user_identifier,
+        amount: req.body.amount
+    }
+
+    try {
+        // transaction in db 
+        await db.$transaction([
+            db.balance.update({
+                where:{
+                    id: paymentInformation.userId,
+                },
+                data: {
+                    amount: {
+                        increment: paymentInformation.amount
+                    }
+                }
+            }),
+            db.onRampTransaction.update({
+                where: {
+                    token: paymentInformation.token
+                },
+                data: {
+                    status: "Success"
+                }
+            })
+        ]);
+        return res.status(200).json({
+            message: "Captured"
+        });
+
+    } catch (error) {
+        console.log(error)
+        return res.status(411).json({
+            message: "Error while processing webhook"
+        })
+    }
+})
 
 app.listen(3002, ()=> {
     console.log("app listening on port 3002")
