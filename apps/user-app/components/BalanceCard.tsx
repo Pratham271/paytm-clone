ba24a9d3c572a24f9a62e24f5b82d676d888191e
Pratham271 <chauhanpratham22@gmail.com>
2024-04-06 21:24:32 +0530

    added the Balance Card

---
+++ apps/user-app/components/BalanceCard.tsx
@@ -0,0 +1,33 @@
+import { Card } from '@repo/ui/card'
+import React from 'react'
+
+const BalanceCard = ({amount, locked}: {amount: number, locked:number}) => {
+  return (
+    <Card title={"Balance"}>
+        <div className="flex justify-between border-b border-slate-300">
+            <div>
+                Unlocked Balance
+            </div>
+            <div>
+               {amount/100} INR
+            </div>
+        </div><div className="flex justify-between border-b border-slate-300">
+            <div>
+                Total Locked Balance
+            </div>
+            <div>
+               {amount/100} INR
+            </div>
+        </div><div className="flex justify-between border-b border-slate-300">
+            <div>
+                Total Balance
+            </div>
+            <div>
+               {(locked+amount)/100} INR
+            </div>
+        </div>
+    </Card>
+  )
+}
+
+export default BalanceCard
