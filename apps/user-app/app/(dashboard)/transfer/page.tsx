b700ea7dc43ebf48c8a1d74af436b6ec8d731b83
Pratham271 <chauhanpratham22@gmail.com>
2024-04-07 14:17:59 +0530

    completed the transfer page

--- apps/user-app/app/(dashboard)/transfer/page.tsx
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -1,35 +1,46 @@
 import React from 'react'
 import AddMoneyCard from '../../../components/AddMoneyCard'
 import BalanceCard from '../../../components/BalanceCard'
-import OnRampTransactions from '../../../components/OnRampTransactions'
+import OnRampTransactions, { OnRampStatus } from '../../../components/OnRampTransactions'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '../../lib/auth'
+import prisma from '@repo/db/client'
 
 enum Status {
   Success,
   Failure,
   Processing
 }
-const page = () => {
-  const date = new Date()
-  const transactions = [
-    {
-      time: date,
-      amount: 200,
-      status: Status.Failure,
-      provider: 'HDFC'
-    },
-    {
-      time: date,
-      amount: 200,
-      status: Status.Processing,
-      provider: 'HDFC'
-    },
-    {
-      time: date,
-      amount: 200,
-      status: Status.Success,
-      provider: 'HDFC'
+async function getBalance(){
+  const session = await getServerSession(authOptions)
+  const balance = await prisma.balance.findFirst({
+    where: {
+      userId: Number(session?.user?.id)
     }
-  ]
+  });
+  return {
+    amount: balance?.amount || 0,
+    locked: balance?.locked || 0
+  }
+}
+
+async function getOnRampTransactions(){
+  const session = await getServerSession(authOptions);
+  const txns = await prisma.onRampTransaction.findMany({
+    where:{
+      userId: Number(session?.user?.id)
+    }
+  })
+  return txns.map((t) => ({
+    time: t.startTime,
+    amount: t.amount,
+    status: t.status as unknown as OnRampStatus,
+    provider: t.provider
+  }))
+}
+const page = async() => {
+  const balance = await getBalance()
+  const transactions = await getOnRampTransactions()
   return (
     <div className="w-screen">
         <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
@@ -40,7 +51,7 @@ const page = () => {
                 <AddMoneyCard />
             </div>
             <div>
-                <BalanceCard amount={100} locked={20} />
+                <BalanceCard amount={balance.amount} locked={balance.locked} />
                 <div className="pt-4">
                     <OnRampTransactions transactions={transactions} />
                 </div>
